ff14321011970cd2d8fec10da0ff553b391c1a37
arcd75 <arcdmaster@gmail.com>
2016-06-26 12:40:52 +0800

    :up:

--- ClassRecordSystem/ClassRecordSystem/Shared/LoginPage.xaml.cs
+++ ClassRecordSystem/ClassRecordSystem/Shared/LoginPage.xaml.cs
@@ -1,5 +1,4 @@
-﻿using DevExpress.Xpf.Core;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ClassRecordSystem
+namespace ClassRecordSystem.Shared
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for LoginPage.xaml
     /// </summary>
-    public partial class MainWindow : DXWindow
+    public partial class LoginPage : Page
     {
-        public MainWindow()
+        public LoginPage()
         {
             InitializeComponent();
         }
